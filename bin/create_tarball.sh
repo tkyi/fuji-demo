844cfad097085336e4e7b80f392477b06be38ca9
Tiffany Kyi <tiffanykyi@gmail.com>
2017-03-07 16:57:38 -0800

    Clean up wording

--- bin/create_tarball.sh
+++ bin/create_tarball.sh
@@ -13,11 +13,11 @@ curl -n -X POST https://api.heroku.com/apps/${HEROKU_APP}/sources \
 PUT_URL=$(cat ./output.txt | jq -r '.source_blob.put_url')
 GET_URL=$(cat ./output.txt | jq -r '.source_blob.get_url')
 
-echo "Upload data.."
+echo "Uploading data.."
 curl "${PUT_URL}" \
   -X PUT -H 'Content-Type:' --data-binary @fuji-demo.tgz
 
-echo "Create the build.."
+echo "Creating the build.."
 BODY=$(jq -n --arg get_url "${GET_URL}" '{"source_blob":{"url": $get_url, "version": "cb6999d361a0244753cf89813207ad53ad906a14"}}')
 
 curl -n -X POST https://api.heroku.com/apps/${HEROKU_APP}/builds \
